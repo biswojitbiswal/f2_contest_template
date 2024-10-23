39e9a885b745d7b06985843567a4b1d52efcff80
Biswojit Biswal <biswojitb474@gmail.com>
2024-10-23 10:40:35 +0530

    Task Completed

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((employee, index) => {
+		if(employee.profession === "developer") {
+		  console.log(employee);
+		}
+	})
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((employee, index) => {
+	  if(employee.profession === "developer"){
+		console.log(employee);
+	  }
+	})
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id: 4, name: "Biswo", age: 23, profession: "developer"});
+	console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  let filteredArr = arr.filter((employee) => (employee.profession !== "admin"));
+	console.log(filteredArr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [
+	  { id: 4, name: "Arun", age: "28", profession: "hr" },
+	  { id: 5, name: "Amar", age: "23", profession: "data analyst" },
+	  { id: 6, name: "Shaban", age: "29", profession: "developer" },
+	];
+  
+	let concatenatedArr = arr.concat(newArr);
+	console.log(concatenatedArr);
 }
