52ae6c3c9aa1c3b2999925ded3cf61db46391591
Biswojit Biswal <biswojitb474@gmail.com>
2024-11-09 13:38:06 +0530

    Some corrections

--- script.js
+++ script.js
@@ -8,56 +8,37 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
-<<<<<<< HEAD
   arr.map(employee => {
     if (employee.profession === "developer") {
       console.log(`id : ${employee.id}, Name : ${employee.name}, Age : ${employee.age}, Profession : ${employee.profession}`);
     }
   });
-=======
-  arr.map((employee, index) => {
-		if(employee.profession === "developer") {
-		  console.log(employee);
-		}
-	})
->>>>>>> 39e9a885b745d7b06985843567a4b1d52efcff80
+
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
-<<<<<<< HEAD
   arr.forEach((employee => {
     if(employee.profession === "developer"){
       console.log(`id : ${employee.id}, Name : ${employee.name}, Age : ${employee.age}, Profession : ${employee.profession}`);
     }
   }))
-=======
-  arr.forEach((employee, index) => {
-	  if(employee.profession === "developer"){
-		console.log(employee);
-	  }
-	})
->>>>>>> 39e9a885b745d7b06985843567a4b1d52efcff80
+  
 }
 
 function addData() {
   //Write your code here, just console.log
-<<<<<<< HEAD
   const newEmployee = {id: 4, name: "Neel", age: "27", profession: "Engineer" };
   arr.push(newEmployee);
   
   arr.forEach((employee) => {
     console.log(`id : ${employee.id}, Name : ${employee.name}, Age : ${employee.age}, Profession : ${employee.profession}`);
   })
-=======
-  arr.push({id: 4, name: "Biswo", age: 23, profession: "developer"});
-	console.log(arr);
->>>>>>> 39e9a885b745d7b06985843567a4b1d52efcff80
+  
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
-<<<<<<< HEAD
   arr = arr.filter((employee) => {
     return employee.profession !== "admin";
   })
@@ -65,15 +46,10 @@ function removeAdmin() {
   arr.forEach((employee) => {
     console.log(`id : ${employee.id}, Name : ${employee.name}, Age : ${employee.age}, Profession : ${employee.profession}`);
   })
-=======
-  let filteredArr = arr.filter((employee) => (employee.profession !== "admin"));
-	console.log(filteredArr);
->>>>>>> 39e9a885b745d7b06985843567a4b1d52efcff80
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
-<<<<<<< HEAD
   const newArr = [
     { id: 5, name: "alice", age: "22", profession: "developer" },
     { id: 6, name: "bob", age: "23", profession: "designer" },
@@ -85,14 +61,5 @@ function concatenateArray() {
   concatenateArr.forEach((employee) => {
     console.log(`id : ${employee.id}, Name : ${employee.name}, Age : ${employee.age}, Profession : ${employee.profession}`);
   })
-=======
-  let newArr = [
-	  { id: 4, name: "Arun", age: "28", profession: "hr" },
-	  { id: 5, name: "Amar", age: "23", profession: "data analyst" },
-	  { id: 6, name: "Shaban", age: "29", profession: "developer" },
-	];
-  
-	let concatenatedArr = arr.concat(newArr);
-	console.log(concatenatedArr);
->>>>>>> 39e9a885b745d7b06985843567a4b1d52efcff80
+
 }
