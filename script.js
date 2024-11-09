07156d55ce650e685263951fd61531618a003b6c
Biswojit Biswal <biswojitb474@gmail.com>
2024-11-09 13:31:18 +0530

    correction

--- script.js
+++ script.js
@@ -8,20 +8,54 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(employee => {
+    if (employee.profession === "developer") {
+      console.log(`id : ${employee.id}, Name : ${employee.name}, Age : ${employee.age}, Profession : ${employee.profession}`);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((employee => {
+    if(employee.profession === "developer"){
+      console.log(`id : ${employee.id}, Name : ${employee.name}, Age : ${employee.age}, Profession : ${employee.profession}`);
+    }
+  }))
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newEmployee = {id: 4, name: "Neel", age: "27", profession: "Engineer" };
+  arr.push(newEmployee);
+  
+  arr.forEach((employee) => {
+    console.log(`id : ${employee.id}, Name : ${employee.name}, Age : ${employee.age}, Profession : ${employee.profession}`);
+  })
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter((employee) => {
+    return employee.profession !== "admin";
+  })
+
+  arr.forEach((employee) => {
+    console.log(`id : ${employee.id}, Name : ${employee.name}, Age : ${employee.age}, Profession : ${employee.profession}`);
+  })
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArr = [
+    { id: 5, name: "alice", age: "22", profession: "developer" },
+    { id: 6, name: "bob", age: "23", profession: "designer" },
+    { id: 7, name: "charlie", age: "24", profession: "manager" },
+  ]
+
+  const concatenateArr = arr.concat(newArr);
+
+  concatenateArr.forEach((employee) => {
+    console.log(`id : ${employee.id}, Name : ${employee.name}, Age : ${employee.age}, Profession : ${employee.profession}`);
+  })
 }
